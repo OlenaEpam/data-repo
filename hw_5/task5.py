98ef5e51ab55e9ba12741d9d621b2b6c45dc33f9
Olena Pavlyushchik <Olena_Pavlyushchik@email.com>
2024-04-18 23:09:02 +0200

    task 5 and task 4 just copied from branch 4

--- hw_5/task5.py
+++ hw_5/task5.py
@@ -1,4 +1,7 @@
 import datetime
+import os
+import sys
+from hw_4.copy_module_4_file import lower_text, text_split_by_sentence, capitalize_sentences
 
 
 class NewsTool:
@@ -38,7 +41,7 @@ class News(Publication):
 
     def print_to_file(self):
         with open('newsfeed.txt', 'a') as file:
-            file.write(f'\nNews-------------\n Text: {self.text} \n City: {self.city} \n '
+            file.write(f'\nNews-------------\n City: {self.text} \n Text: {self.city} \n '
                        f'Date of publication: {datetime.date.today()}')
 
 
@@ -72,5 +75,56 @@ class Event(Publication):
                        f'\n Event date: {self.event_date}')
 
 
-a = NewsTool(input('Select type: 1 - News, 2 - Private add, 3 - Event: '))
-a.start_program()
+# a = NewsTool(input('Select type: 1 - News, 2 - Private add, 3 - Event: '))
+# a.start_program()
+
+
+class InputFromFile:
+    def __init__(self, file_input):
+        self.file_input = file_input
+
+    def normalize_text(self, some_text):
+        lowered_text = lower_text(some_text)
+        split_text = text_split_by_sentence(lowered_text)
+        capitalized = capitalize_sentences(split_text)
+        return capitalized
+
+    def start_program_2(self):
+        with open(self.file_input, 'r') as file:
+            lines = file.readlines()
+        for line in lines:
+            if line == '1\n':
+                index_of_news = lines.index(line)
+                text = self.normalize_text(lines[index_of_news+1].strip())
+                city = self.normalize_text(lines[index_of_news+2].strip())
+                b = News(text, city)
+                b.print_to_file()
+            elif line == '2\n':
+                index_of_add = lines.index(line)
+                text = self.normalize_text(lines[index_of_add+1].strip())
+                expiration_date = lines[index_of_add+2].strip()
+                b = PrivateAdd(expiration_date, text)
+                b.print_to_file()
+            elif line == '3\n':
+                index_of_event = lines.index(line)
+                text = self.normalize_text(lines[index_of_event+1].strip())
+                ticket_price = lines[index_of_event+2].strip()
+                event_date = lines[index_of_event+3].strip()
+                b = Event(text, ticket_price, event_date)
+                b.print_to_file()
+
+
+if len(sys.argv) > 1:
+    file_itself = sys.argv[1]
+else:
+    file_itself = '/Users/Olena_Pavlyushchik/Library/CloudStorage/OneDrive-EPAM/for_data_python'
+    files = os.listdir(file_itself)
+    for file_name in files:
+        if file_name.endswith('.txt'):
+            file_itself = os.path.join(file_itself, file_name)
+
+c = InputFromFile(file_itself)
+c.start_program_2()
+os.remove(file_itself)
+
+# parameter can be used - /Users/Olena_Pavlyushchik/data-repo/hw_5/file.txt
